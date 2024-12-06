59ededbaefc434567ab1690cea803a7e9c50e25c
KyleRabkin61 <kylerabkin6161@gmail.com>
2024-12-06 13:26:25 -0500

    List Rendering

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart: 0,
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
-            inventory: 10
+            inStock: 10,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green'},
+                { id: 2235, color: 'blue'}
+            ]
         }
     }
 })
